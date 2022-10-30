00130da62f3f06249def43ec4d8e287f0a1b12cf
Mohammod <hafezmohammod0011@gmail.com>
2022-10-30 18:58:16 +0600

    new

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import AddUser from './components/AddUser';
 import Home from './components/Home';
+import Uptade from './components/Uptade';
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path : '/',
-      element : <Home></Home>
+      element : <Home></Home>,
+      loader : () => fetch('http://localhost:5000/users')
     },
     {
       path : '/users/add',
       element : <AddUser></AddUser>
+    },
+    {
+      path : '/uptade/:id',
+      element : <Uptade></Uptade>,
+      loader : ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
     }
   ])
 
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 
 const AddUser = () => {
-    const [user, setUser] = useState({name : 'helloWorld', email : 'h@gmail.com'})
+    const [user, setUser] = useState({});
+
+
     const handleAddUser = event => {
         event.preventDefault();
         console.log(user)
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    alert('User Added Successfully');
+                    event.target.reset();
+                }
+            })
     }
 
     const handleOnBlur = event => {
         const value = event.target.value;
         const field = event.target.name;
-        const newUser = {...user};
+        const newUser = { ...user };
         newUser[field] = value;
-        setUser(newUser)
+        setUser(newUser);
     }
 
     return (
@@ -25,10 +42,13 @@ const AddUser = () => {
                 <br />
                 <input onBlur={handleOnBlur} type="text" name="phone" />
                 <br />
-                <input onBlur={handleOnBlur}  type="email" name="email" />
+                <input onBlur={handleOnBlur} type="email" name="email" />
                 <br />
                 <button type="submit">Add User</button>
             </form>
+            <div>
+                
+            </div>
         </div>
     );
 };
