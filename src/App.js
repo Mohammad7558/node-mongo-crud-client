dbd61dca04c5961f40cffd74672934b6e57028f6
Mohammod <hafezmohammod0011@gmail.com>
2022-11-02 23:34:12 +0600

    node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import AddUser from './components/AddUser';
-import Home from './components/Home';
-import Uptade from './components/Uptade';
+
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path : '/',
-      element : <Home></Home>,
-      loader : () => fetch('http://localhost:5000/users')
+      element : ''
     },
     {
-      path : '/users/add',
-      element : <AddUser></AddUser>
-    },
-    {
-      path : '/uptade/:id',
-      element : <Uptade></Uptade>,
-      loader : ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
+      path : '/user/add',
+      element : ''
     }
   ])
 
